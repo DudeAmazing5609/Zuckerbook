8286d07988e6fa561606a88951415622498a49de
DudeAmazing5609 <122160269+DudeAmazing5609@users.noreply.github.com>
2025-03-22 17:50:27 +0530

    V1.4 - Internal API

---
+++ ZuckerbookAPI.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+
+export function AppendPost(postername, posternumber, title, body, postnumber){
+	var posthtml = `<div class="post">
+		<p>[${postername} - ${posternumber}]</p>
+		<h2>${title}</h2>
+		<p>${body}</p><br>
+		<p>[${postnumber}]</p>
+		</div>
+		`;
+
+	const postsdiv = document.getElementById("posts");
+
+	postsdiv.innerHTML = posthtml + postsdiv.innerHTML
+
+}
+
+export function firestoreDB(){
+	
+	const firebaseConfig = {
+		apiKey: "AIzaSyBOavStuEiXF--gJHoLt0q11nlomkx9pqI",
+		authDomain: "zuckerbook-ee135.firebaseapp.com",
+		projectId: "zuckerbook-ee135",
+		storageBucket: "zuckerbook-ee135.firebasestorage.app",
+		messagingSenderId: "1044679454146",
+		appId: "1:1044679454146:web:084841931bf2dd2ca57a5b"
+	};
+
+	const app = initializeApp(firebaseConfig);
+	return getFirestore(app);
+}
+
+export function getQueryParam(param) {
+	var urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get(param);
+}
