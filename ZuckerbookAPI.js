e12dccb01138ab10d2145beff30482c802c0de49
DudeAmazing5609 <122160269+DudeAmazing5609@users.noreply.github.com>
2025-03-27 17:49:37 +0530

    V3

--- ZuckerbookAPI.js
+++ ZuckerbookAPI.js
@@ -1,20 +1,46 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
+
+export function authorised(){
+	if(sessionStorage.getItem('uname') == null || sessionStorage.getItem('unum') == null){
+		console.log("Not authorised");
+		return false;
+	} else {
+		console.log("Authorised");
+		return true;
+	}
+}
+
 export function AppendPost(postername, posternumber, title, body, postnumber, likes, dislikes) {
 
-	var posthtml = `<div class="post">
+	var posthtml = `
+		<div class="post">
 		<p>[${postername} - ${posternumber}]</p>
 		<h2>${title}</h2>
 		<p>${body}</p><br>
-		<p style="width: 10%; float: left;" onclick="like('${postnumber}')">👍${likes}</p><p onclick="dislike('${postnumber}')">👎${dislikes}</p>
+		<p style="width: 10%; float: left;" onclick="like('${postnumber}')">👍${likes}</p><p onclick="dislike('${postnumber}')">👎${dislikes}</p><p onclick="window.location.href = 'commentpage.html?title=${title}&body=${body}&pnum=${postnumber}'">💬</p>
 		<p>[${postnumber}]</p>
 		</div>
-		`;
+	`;
+
+	var posthtml_notAuthorised = `
+		<div class="post">
+		<p>[${postername} - ${posternumber}]</p>
+		<h2>${title}</h2>
+		<p>${body}</p><br>
+		<p style="width: 10%; float: left;">👍${likes}</p><p>👎${dislikes}</p>
+		<p>[${postnumber}]</p>
+		</div>
+	`
 
 	const postsdiv = document.getElementById("posts");
 
-	postsdiv.innerHTML = posthtml + postsdiv.innerHTML
+	if(authorised()){
+		postsdiv.innerHTML = posthtml + postsdiv.innerHTML
+	} else {
+		postsdiv.innerHTML = posthtml_notAuthorised + postsdiv.innerHTML
+	}
 
 }
 
